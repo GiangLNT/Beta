3eb423034fad31772f78d7da057c03ebff663bc9
GiangLNT <truonggiang.n.le@gmail.com>
2020-11-01 15:43:34 +0700

    Update Captcha

--- forms/contact.php
+++ forms/contact.php
@@ -36,6 +36,7 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  
+  $contact->recaptcha_secret_key = '6Leqw90ZAAAAACk7MAckvzplcaQp9KXM3Kw5WARD';
   echo $contact->send();
 ?>
